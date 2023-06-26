dc09b39c56752fd82ad3f36ed5e9e241dedb8f6e
Tronikel <contactdonatas@gmail.com>
2023-06-26 20:14:26 +0300

    join a room

--- extension/popup/src/routes/room_join/index.tsx
+++ extension/popup/src/routes/room_join/index.tsx
@@ -1,13 +1,42 @@
-import { Group, Stack, Title } from "@mantine/core";
-import React from "react";
+import { Button, Group, Stack, TextInput, Title } from "@mantine/core";
+import React, { useState, ChangeEvent } from "react";
+import { useNavigate } from "react-router-dom";
+import urlbat from "urlbat";
 import GoBack from "~/popup/components/GoBack";
 
 export default function RoomJoin() {
+    const navigate = useNavigate();
+
+    const [id, setId] = useState("");
+
+    function onChangeInput(e: ChangeEvent<HTMLInputElement>) {
+        setId(e.target.value);
+    }
+
+    function onJoin() {
+        if (!id) return;
+        const url = urlbat("/room/:id", { id });
+        navigate(url);
+    }
+
     return (
         <Stack>
             <Group>
                 <GoBack />
                 <Title order={4}>Join a room</Title>
+            </Group>
+
+            <Group mt="md" sx={{ justifyContent: "center" }}>
+                <TextInput
+                    label="Id"
+                    placeholder="Enter Id"
+                    value={id}
+                    onChange={onChangeInput}
+                />
+
+                <Button onClick={onJoin} sx={{ alignSelf: "flex-end" }}>
+                    Join
+                </Button>
             </Group>
         </Stack>
     );
