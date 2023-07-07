06095b0761f0ba7b78f4cee7502861b189a82cae
Tronikel <contactdonatas@gmail.com>
2023-07-07 23:13:58 +0300

    boilerplate for background script

---
+++ extension/bg/src/index.ts
@@ -0,0 +1,8 @@
+import { io } from "socket.io-client";
+
+export const socket = io(
+    import.meta.env.MODE === "production"
+        ? "https://twopair.tronikel.lt"
+        : "http://localhost:3000",
+    { ackTimeout: 5e3 }
+);
