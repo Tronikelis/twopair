f4616f612eec2838ea7c9646e923895268d4e172
Tronikel <contactdonatas@gmail.com>
2023-07-09 01:00:03 +0300

    apology letter

--- extension/popup/src/routes/idx/index.tsx
+++ extension/popup/src/routes/idx/index.tsx
@@ -59,6 +59,13 @@ export default function Idx() {
         navigate(urlbat("/room/:id", { id: lastRoomId }));
     }
 
+    // note: status.syncingId is independent for every tab
+    // meaning we can check if we are currently globally syncing with "status.syncing"
+    // and if that syncing is happening in this tab if the "status.syncingId" is truthy
+
+    // if we are syncing, but not in this tab and we want to leave:
+    // we need to somehow force the user to go to the tab where the syncing is happening
+    // and leave the room from that tab (content_script on THAT SPECIFIC needs to remove event listeners)
     const canCreate = !!status && !status.syncing && !status.syncingId;
     const canLeave = !(!!status && status.syncing && !status.syncingId);
 
