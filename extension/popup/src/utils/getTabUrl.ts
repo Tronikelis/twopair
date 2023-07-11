7cb09503b6bb7a2ca0732b87aeb04f5f265fe289
Tronikel <contactdonatas@gmail.com>
2023-07-11 21:04:09 +0300

    it appears that activeTab is not needed, the all_urls gives me all the permissions i need?

--- extension/popup/src/utils/getTabUrl.ts
+++ extension/popup/src/utils/getTabUrl.ts
@@ -1,5 +1,6 @@
 import browser from "webextension-polyfill";
 
+/** this function currently returns an empty string if we can't get an url from the active tab */
 export default async function getTabUrl(): Promise<string> {
     const [currentTab] = await browser.tabs.query({ active: true, currentWindow: true });
     if (!currentTab) {
